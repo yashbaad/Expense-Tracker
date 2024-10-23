7c9e67e10a5b75c6bf0729d24bb2fa2471dd6b24
Yash <yashbaad@gmail.com>
2024-10-23 16:13:42 +0530

    Initial commit

---
+++ src/test/java/com/yashbaad/expensetracker/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashbaad.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
